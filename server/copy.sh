91c134ff86b2a611c768948ac2f240c935814eb3
Unknown <lmoesman@avans.nl>
2019-04-18 14:30:50 +0200

    Fixed copy script Docker

--- server/copy.sh
+++ server/copy.sh
@@ -1,10 +1,5 @@
-read -p "Source path (full path): "  srcPath
+read -p "Source path (absolute path): "  srcPath
 read -p "Destination path *optional [current dict]: " dstPath
 dstPath=${dstPath:-.}
 id=$(docker ps -asqf "name=sendlab-ubuntu")
-if [ -z "$srcPath" ]
-then
-      echo "Error: Source path can't be empty"
-else
-      echo $(docker cp $id:$srcPath $dstPath)
-fi
+echo $(docker cp $id:$srcPath $dstPath)
