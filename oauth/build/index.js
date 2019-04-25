6d78cf383644f81ea43fc57cd0633819d4ba57f3
Rick Verstraten <r.verstraten@gobright.com>
2019-04-25 14:57:49 +0200

    removed 2fa

--- oauth/build/index.js
+++ oauth/build/index.js
@@ -14,17 +14,14 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-//import { users } from '../shadow/users.js';
-var users = [{
+var user = {
   userName: "root",
   id: "b5bb20650eece726645891c81b9e9618",
   role: "admin",
-  passwordHashed: "c6f038f9265cd8d9ecb841948c7086da3e716b2c5e95a1e9a1a8d20d63ff09627913cc88a7d479a8fde261de6ee650702dfa5c669d7583a3c9f7d09457f07a80",
-  salt: "492330363fbed184b7f574e84d283f4b3507d7a8a3d7e2c9635d08529c6c3716346f3eb5b9397ec03596ffa043c18fb4ec7e1ee53cfed9fa9e994bb1970b326d",
-  twoWayAuthSeed: "07414149916e647534c77133e4369496",
-  //base32  A5AUCSMRNZSHKNGHOEZ6INUUSY,
+  passwordHashed: "6f9d132c4e31758c52fe8d4a53024b730f437023bfbeb44f4106b2e386de8431abf21cb9f9be7dea86bbf7b54968a02dc31b2c194a5f705748af52d969678c46",
+  salt: "aa74a24d8dca1d927f9eafd9bb237ba5cc4f2b1a28eceb21f8b5657a71bd8d5c5e612c35aec15011f5b5dfca031cab2b563711dad243bb4dc28eceb95e067f27d773378657feaca25f8606c131b5a330b517292801b4ba68ac571c8182149e82b63179d456ad2b10eb062e96b9edc3ae774316fdd3375162e5cdb9c8254b47b9",
   refresh_token: null
-}];
+};
 const app = (0, _express.default)();
 const port = 8080;
 app.use(_bodyParser.default.json());
@@ -53,12 +50,12 @@ function hash(password, salt) {
 }
 
 function createTokens(id, role) {
-  console.log(_jwt.private_key);
   const access_token = jwt.sign({
     id,
     role
   }, _jwt.private_key, {
-    algorithm: "HS512"
+    algorithm: "HS512",
+    expiresIn: '600s'
   });
 
   const refresh_token = _crypto.default.randomBytes(32).toString("hex");
@@ -72,67 +69,59 @@ function createTokens(id, role) {
 app.post("/login", (request, response) => {
   const userName = request.body.userName;
   const password = request.body.password;
-  const timeCode = Math.trunc(Date.now() / 30000).toString(16).padStart(16, '0');
-  const hotp = request.body.hotp;
-  users = users.map(user => {
-    if (emailCompair(request.body.userName, userName)) {
-      if (hash(password, user.salt).hash === user.passwordHashed) {
-        const HMAC = _crypto.default.createHmac('sha1', new Buffer(user.twoWayAuthSeed, "hex")).update(new Buffer(timeCode, "hex")).digest();
 
-        const HOTP = truncate(HMAC).toString().padStart(6, "0");
-
-        if (hotp === HOTP) {
-          const tokens = createTokens(user.id, user.role);
-          user.refresh_token = tokens.refresh_token;
-          response.send(tokens);
-          return user;
-        }
-
-        response.status(500).send("nope");
-        return user;
-      }
-
-      response.status(500).send("nope");
+  if (emailCompair(user.userName, userName)) {
+    if (hash(password, user.salt).hash === user.passwordHashed) {
+      const tokens = createTokens(user.id, user.role);
+      user.refresh_token = tokens.refresh_token;
+      response.send(tokens);
       return user;
     }
 
     response.status(500).send("nope");
     return user;
-  });
+  }
+
   response.status(500).send("nope");
   return user;
 });
 app.post("/refresh", (request, response) => {
   const refreshToken = request.body.refresh_token;
   const userName = request.body.userName;
-  users = users.map(user => {
-    if (emailCompair(request.body.userName, userName)) {
-      if (user.refresh_token === refreshToken) {
-        const tokens = createTokens(user.id, user.role);
-        user.refresh_token = tokens.refresh_token;
-        response.send(tokens);
-        return user;
-      }
+  if (!refreshToken || !userName) return response.send("nope");
 
-      response.status(500).send("nope");
+  if (emailCompair(user.userName, userName)) {
+    if (user.refresh_token === refreshToken) {
+      const tokens = createTokens(user.id, user.role);
+      user.refresh_token = tokens.refresh_token;
+      response.send(tokens);
       return user;
     }
 
     response.status(500).send("nope");
     return user;
-  });
+  }
+
   response.status(500).send("nope");
   return user;
+});
+app.post("/validate", (request, response) => {
+  const accessToken = request.body.access_token;
+  if (!accessToken) return response.send("nope");
+
+  try {
+    jwt.verify(accessToken, _jwt.private_key, {
+      algorithm: "HS512",
+      expiresIn: '600s'
+    });
+    response.send("good job");
+  } catch (_unused) {
+    response.send("nope");
+  }
 });
 
 function emailCompair(email1, email2) {
   const e1 = email1.trim();
   const e2 = email2.trim();
   return e1.toLocaleLowerCase() === e2.toLocaleLowerCase();
-}
-
-function truncate(hmac) {
-  let offset = hmac[19] & 0xf;
-  const truncatedHash = (hmac[offset] & 0x7f) << 24 | (hmac[offset + 1] & 0xff) << 16 | (hmac[offset + 2] & 0xff) << 8 | hmac[offset + 3] & 0xff;
-  return truncatedHash % Math.pow(10, 6);
 }
--- oauth/src/index.js
+++ oauth/src/index.js
@@ -2,21 +2,18 @@ import bodyParser from "body-parser";
 import crypto from "crypto";
 import express from "express";
 
-//import { users } from '../shadow/users.js';
 import { private_key } from '../shadow/jwt.js';
 import * as jwt from 'jsonwebtoken';
 
-var users = [
-    {
-        userName: "root",
-        id: "b5bb20650eece726645891c81b9e9618",
-        role: "admin",
-        passwordHashed: "c6f038f9265cd8d9ecb841948c7086da3e716b2c5e95a1e9a1a8d20d63ff09627913cc88a7d479a8fde261de6ee650702dfa5c669d7583a3c9f7d09457f07a80",
-        salt: "492330363fbed184b7f574e84d283f4b3507d7a8a3d7e2c9635d08529c6c3716346f3eb5b9397ec03596ffa043c18fb4ec7e1ee53cfed9fa9e994bb1970b326d",
-        twoWayAuthSeed: "07414149916e647534c77133e4369496", //base32  A5AUCSMRNZSHKNGHOEZ6INUUSY,
-        refresh_token: null
-    }
-]
+var user = {
+    userName: "root",
+    id: "b5bb20650eece726645891c81b9e9618",
+    role: "admin",
+    passwordHashed: "6f9d132c4e31758c52fe8d4a53024b730f437023bfbeb44f4106b2e386de8431abf21cb9f9be7dea86bbf7b54968a02dc31b2c194a5f705748af52d969678c46",
+    salt: "aa74a24d8dca1d927f9eafd9bb237ba5cc4f2b1a28eceb21f8b5657a71bd8d5c5e612c35aec15011f5b5dfca031cab2b563711dad243bb4dc28eceb95e067f27d773378657feaca25f8606c131b5a330b517292801b4ba68ac571c8182149e82b63179d456ad2b10eb062e96b9edc3ae774316fdd3375162e5cdb9c8254b47b9",
+    refresh_token: null
+}
+
 
 const app = express();
 const port = 8080;
@@ -49,11 +46,10 @@ function hash(password, salt) {
 }
 
 function createTokens(id, role) {
-    console.log(private_key);
     const access_token = jwt.sign({
         id,
         role
-    }, private_key, { algorithm: "HS512" });
+    }, private_key, { algorithm: "HS512",expiresIn: '600s' });
 
     const refresh_token = crypto.randomBytes(32).toString("hex");
     return {
@@ -65,30 +61,17 @@ function createTokens(id, role) {
 app.post("/login", (request, response) => {
     const userName = request.body.userName;
     const password = request.body.password;
-    const timeCode = Math.trunc(Date.now() / 30000).toString(16).padStart(16,'0');
-    const hotp = request.body.hotp;
 
-    users = users.map((user) => {
-        if (emailCompair(request.body.userName, userName)) {
-            if (hash(password, user.salt).hash === user.passwordHashed) {
-                const HMAC = crypto.createHmac('sha1', new Buffer(user.twoWayAuthSeed,"hex")).update(new Buffer(timeCode, "hex")).digest();
-                const HOTP = truncate(HMAC).toString().padStart(6,"0");
-
-                if(hotp === HOTP){
-                    const tokens = createTokens(user.id, user.role);
-                    user.refresh_token = tokens.refresh_token;
-                    response.send(tokens);
-                    return user;
-                }
-                response.status(500).send("nope");
-                return user;
-            }
-            response.status(500).send("nope");
+    if (emailCompair(user.userName, userName)) {
+        if (hash(password, user.salt).hash === user.passwordHashed) {
+            const tokens = createTokens(user.id, user.role);
+            user.refresh_token = tokens.refresh_token;
+            response.send(tokens);
             return user;
         }
         response.status(500).send("nope");
         return user;
-    });
+    }
     response.status(500).send("nope");
     return user;
 });
@@ -96,38 +79,38 @@ app.post("/login", (request, response) => {
 app.post("/refresh", (request, response)=>{
     const refreshToken = request.body.refresh_token;
     const userName = request.body.userName;
-
-    users = users.map(user =>{
-        if (emailCompair(request.body.userName, userName)) {
-            if (user.refresh_token === refreshToken) {
-                const tokens = createTokens(user.id, user.role);
-                user.refresh_token = tokens.refresh_token;
-                response.send(tokens);
-                return user;
-            }
-            response.status(500).send("nope");
+    if(!refreshToken || !userName)
+        return response.send("nope");
+    if (emailCompair(user.userName, userName)) {
+        if (user.refresh_token === refreshToken) {
+            const tokens = createTokens(user.id, user.role);
+            user.refresh_token = tokens.refresh_token;
+            response.send(tokens);
             return user;
         }
         response.status(500).send("nope");
         return user;
-    });
+    }
     response.status(500).send("nope");
     return user;
 })
 
+app.post("/validate", (request, response)=>{
+    const accessToken = request.body.access_token;
+    if(!accessToken)
+        return response.send("nope");
+    
+    
+    try {
+        jwt.verify(accessToken,private_key,{algorithm:"HS512", expiresIn: '600s'})
+        response.send("good job");
+    }catch {
+        response.send("nope");
+    }
+})
 function emailCompair(email1, email2) {
     const e1 = email1.trim();
     const e2 = email2.trim();
 
     return (e1.toLocaleLowerCase() === e2.toLocaleLowerCase());
-}
-
-function truncate(hmac) {
-    let offset = hmac[19] & 0xf;
-    const truncatedHash =
-        (hmac[offset] & 0x7f) << 24 |
-        (hmac[offset + 1] & 0xff) << 16 |
-        (hmac[offset + 2] & 0xff) << 8 |
-        (hmac[offset + 3] & 0xff)
-    return truncatedHash % Math.pow(10, 6);
 }
