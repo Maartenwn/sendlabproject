34951c45394d6f32d5a77cdc4c748452fffa3a31
Unknown <lmoesman@avans.nl>
2019-05-14 15:03:08 +0200

    Fixed new oauth

--- upload-cloud-test.sh
+++ upload-cloud-test.sh
@@ -2,7 +2,7 @@ find . -name "node_modules" -type d -prune -exec rm -rf '{}' +
 
 sftp maurice@145.48.205.52 <<EOF
 put -r database-link
-put -r event-generator 
+put -r event-generator-handler 
 put -r mqtt
 put -r node
 put -r oauth 
