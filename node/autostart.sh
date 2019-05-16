07138a306b738aa8acaa45dd7c1c99ed30d538a2
Unknown <maartenwn@gmail.com>
2019-05-16 16:06:21 +0200

    Changed node/autostart to also start event generator

--- node/autostart.sh
+++ node/autostart.sh
@@ -3,10 +3,13 @@ npm install rest-full --production
 npm install oauth --production
 npm install database-link --production
 npm install test-apps/mqtt-test --production
+npm install event-generator-handler --production
 
 pm2 start index.js --name rest-full --cwd /root/rest-full/
 pm2 start index.js --name oauth --cwd /root/oauth/
 pm2 start index.js --name database-link --cwd /root/database-link/
 pm2 start index.js --name mqtt-test --cwd /root/test-apps/mqtt-test/
+pm2 start index.js --name event-generator-handler --cwd /root/event-generator-handler/
+
 pm2 save
 /bin/bash
