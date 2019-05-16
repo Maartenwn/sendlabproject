d32e3684fa046fad808d387d8578c07d3a96a457
Unknown <lmoesman@avans.nl>
2019-05-16 15:38:48 +0200

    Corrected mistake that was made in docs

--- event-generator-handler/sensor-manager.js
+++ event-generator-handler/sensor-manager.js
@@ -10,18 +10,22 @@ const checkSensors = ({ identifier, sensors }) => {
     updateData(identifier, sensors)
     return;
   }
-  
+
   const deviceSensors = devices[identifier];
 
   sensors.forEach((item) => {
     const key = Object.keys(item)[0];
     const value = item[key];
-    if (deviceSensors[key] == false && value == true) {
+
+    const valid = (typeof value == 'boolean') ? true : 'true';
+    const invalid = (typeof value == 'boolean') ? false : 'false';
+
+    if (deviceSensors[key] == invalid && value == valid) {
       callback({
         type: "sensor-on",
         message: `${key} has turned on.`
       }, identifier);
-    } else if (deviceSensors[key] == true && value == false) {
+    } else if (deviceSensors[key] == valid && value == invalid) {
       callback({
         type: "sensor-off",
         message: `${key} has turned off.`
