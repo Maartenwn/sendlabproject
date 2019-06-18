c08c687a6db8de03d07349d44b18ba6e3acdc1ee
Unknown <maartenwn@gmail.com>
2019-06-18 13:34:15 +0200

    added future database connection to current

--- rest-full/routes/current.js
+++ rest-full/routes/current.js
@@ -1,10 +1,22 @@
+const database = require('../../database/api');
+
+
 const express = require("express");
 const router = express.Router();
 
+
+
 router.get('/device/:deviceId/data',(req,res) => {
     let deviceId = req.params.deviceId;
-    res.status(200).json({
-        test: "current"
-    })
+    
+    //await database.getCurrentData().then(result => {
+        res.status(200).json({
+            "test":"current"
+        })
+    //}).catch(error =>{
+    //    res.status(500).json({
+    //        error
+    //    })
+    //});    
 })  
 module.exports = router;
