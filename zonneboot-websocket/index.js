73977f5f880c1083ff78ce0c3789ec4baa3cb653
Unknown <maartenwn@gmail.com>
2019-06-18 15:45:24 +0200

    Added https

--- zonneboot-websocket/index.js
+++ zonneboot-websocket/index.js
@@ -21,23 +21,22 @@ const brokerOptions = {
 const dataTopic = 'data/zonneboot';
 
 const WebSocket = require('ws')
-//const httpsServer = require('https');
+const https = require('https');
 
-//var privateKey = fs.readFileSync('ssl-cert/privkey.pem', 'utf8');
-//var certificate = fs.readFileSync('ssl-cert/fullchain.pem', 'utf8');
+var privateKey = fs.readFileSync('ssl-sert/privkey.pem', 'utf8');
+var certificate = fs.readFileSync('ssl-sert/fullchain.pem', 'utf8');
 
-//var credentials = { key: privateKey, cert: certificate };
-//var httpsServer = https.createServer(credentials);
-//httpsServer.listen(8443);
+var credentials = { key: privateKey, cert: certificate };
+var httpsServer = https.createServer(credentials);
+httpsServer.listen(8080);
 
-// var WebSocketServer = require('ws').Server;
-// var wss = new WebSocketServer({
-//     server: httpsServer
-//});
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({
+    server: httpsServer,
+});
 
-//sudo certbot certonly --standalone -d example.com 
 
-const wss = new WebSocket.Server({ port: 8080 })
+//const wss = new WebSocket.Server({ port: 8080 })
 
 wss.on('connection', ws => {
     console.log("Connected");
